63ae6de5c2056c9a7714184d035ee5fd518de85e
Brian Takita <brian.takita@gmail.com>
2021-05-30 13:39:32 -0400

    ## 3.0.0
    ### Major Changes
    
    - \$ suffix for stores
    - \_ suffix for factory functions
    
    ### Patch Changes
    
    - Updated dependencies [undefined]
    - Updated dependencies [undefined]
    - @ctx-core/object@19.0.0
    - @ctx-core/store@25.0.0

--- src/event_log$_b.ts
+++ src/event_log$_b.ts
@@ -1,16 +1,16 @@
 import { _b, assign } from '@ctx-core/object'
 import { _readable_set_ctx$, Readable$ } from '@ctx-core/store'
 import type { event_log_Ctx } from './event_log_Ctx'
-const key = 'event_log'
-export function event_log_b(ctx:event_log_Ctx) {
+const key = 'event_log$'
+export function event_log$_b(ctx:event_log_Ctx) {
 	return _b<event_log_Ctx, typeof key>(key, ()=>{
-		const $event_log:$event_log_T = []
+		const $event_log:event_log_T = []
 		let limit = 10_000
-		const { store: event_log, set } = _readable_set_ctx$<$event_log_T>($event_log)
+		const { store: event_log, set } = _readable_set_ctx$<event_log_T>($event_log)
 		return assign(event_log, {
 			add,
 			set_limit,
-		}) as event_log_T
+		}) as event_log$_T
 		function add(record:event_log_record_T) {
 			$event_log.unshift(record)
 			if ($event_log.length > limit) {
@@ -24,10 +24,10 @@ export function event_log_b(ctx:event_log_Ctx) {
 	})(ctx)
 }
 export interface event_log_record_T<I extends any = any> extends Record<string, I> {}
-export type $event_log_T = event_log_record_T[]
-export type event_log_add_T = (record:event_log_record_T)=>$event_log_T
+export type event_log_T = event_log_record_T[]
+export type event_log_add_T = (record:event_log_record_T)=>event_log_T
 export type event_log_set_limit_T = (in_limit:number)=>void
-export interface event_log_T extends Readable$<$event_log_T> {
+export interface event_log$_T extends Readable$<event_log_T> {
 	add:event_log_add_T
 	set_limit:event_log_set_limit_T
 }
