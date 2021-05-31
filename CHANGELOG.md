313358aac64d13d1377556431b306ef1d00319b3
Brian Takita <brian.takita@gmail.com>
2021-05-31 15:57:13 -0400

    ## 3.0.2
    ### Patch Changes
    
    - _b->be_
    - Updated dependencies [undefined]
    - @ctx-core/object@19.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,13 @@
 # @ctx-core/event-log
 
+## 3.0.2
+
+### Patch Changes
+
+- _b->be_
+- Updated dependencies [undefined]
+  - @ctx-core/object@19.0.2
+
 ## 3.0.1
 
 ### Patch Changes
--- src/event_log$_b.ts
+++ src/event_log$_b.ts
@@ -1,9 +1,9 @@
-import { _b, assign } from '@ctx-core/object'
+import { be_, assign } from '@ctx-core/object'
 import { readable$_set_ctx_, Readable$ } from '@ctx-core/store'
 import type { event_log_Ctx } from './event_log_Ctx'
 const key = 'event_log$'
 export function event_log$_b(ctx:event_log_Ctx) {
-	return _b<event_log_Ctx, typeof key>(key, ()=>{
+	return be_<event_log_Ctx, typeof key>(key, ()=>{
 		const $event_log:event_log_T = []
 		let limit = 10_000
 		const { store: event_log, set } = readable$_set_ctx_<event_log_T>($event_log)
