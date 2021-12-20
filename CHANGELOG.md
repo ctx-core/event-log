85a42a609c18475ff014d95019807a790197c879
Brian Takita <brian.takita@gmail.com>
2021-12-20 17:32:55 -0600

    ## 7.0.0
    ### Major Changes
    
    - using @ctx-core/nanostores for stores instead of @ctx-core/store,@ctx-core/svelte

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # @ctx-core/event-log
 
+## 7.0.0
+
+### Major Changes
+
+- using @ctx-core/nanostores for stores instead of @ctx-core/store,@ctx-core/svelte
+
 ## 6.0.8
 
 ### Patch Changes
--- src/event_log$_b.ts
+++ src/event_log$_b.ts
@@ -1,11 +1,11 @@
 import { be_, assign, Ctx } from '@ctx-core/object'
-import { readable$_set_ctx_, Readable$ } from '@ctx-core/store'
+import { split_atom$, ReadableAtom$ } from '@ctx-core/nanostores'
 const key = 'event_log$'
 export function event_log$_b(ctx:Ctx) {
 	return be_<event_log$_T>(key, ()=>{
 		const event_log:event_log_T = []
 		let limit = 10_000
-		const { store: event_log$, set } = readable$_set_ctx_<event_log_T>(event_log)
+		const [event_log$, set] = split_atom$<event_log_T>(event_log)
 		return assign(event_log$, {
 			add,
 			set_limit,
@@ -26,7 +26,7 @@ export interface event_log_record_T<I extends any = any> extends Record<string, 
 export type event_log_T = event_log_record_T[]
 export type event_log_add_T = (record:event_log_record_T)=>event_log_T
 export type event_log_set_limit_T = (in_limit:number)=>void
-export interface event_log$_T extends Readable$<event_log_T> {
+export interface event_log$_T extends ReadableAtom$<event_log_T> {
 	add:event_log_add_T
 	set_limit:event_log_set_limit_T
 }
