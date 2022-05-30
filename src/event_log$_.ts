5a43001a1ab31b218a24ee9429c18196c5c7fde8
Brian Takita <brian.takita@gmail.com>
2022-05-30 01:40:27 -0500

    update dependencies

--- src/event_log$_.ts
+++ src/event_log$_.ts
@@ -1,9 +1,9 @@
 import { assign, be_ } from '@ctx-core/object'
-import { ReadableAtom$, split_atom$ } from '@ctx-core/nanostores'
+import { ReadableAtom_, split_atom_ } from '@ctx-core/nanostores'
 export const event_log$_ = be_<event_log$_T>('event_log$', ()=>{
 	const event_log:event_log_T = []
 	let limit = 10_000
-	const [event_log$, set] = split_atom$<event_log_T>(event_log)
+	const [event_log$, set] = split_atom_<event_log_T>(event_log)
 	return assign(event_log$, {
 		add,
 		set_limit,
@@ -23,7 +23,7 @@ export interface event_log_record_T<I extends any = any> extends Record<string, 
 export type event_log_T = event_log_record_T[]
 export type event_log_add_T = (record:event_log_record_T)=>event_log_T
 export type event_log_set_limit_T = (in_limit:number)=>void
-export interface event_log$_T extends ReadableAtom$<event_log_T> {
+export interface event_log$_T extends ReadableAtom_<event_log_T> {
 	add:event_log_add_T
 	set_limit:event_log_set_limit_T
 }
