557beed05b2fbd5d1225e68cd7d40ed989eb8530
Brian Takita <brian.takita@gmail.com>
2023-10-06 02:11:01 -0400

    diff --git a/CHANGELOG.md b/CHANGELOG.md index 45a63a4..c44ae33 100644 --- a/CHANGELOG.md ++ b/CHANGELOG.md @@ -1,5 +1,13 @@ # @ctx-core/event-log
    ## 9.1.27
    
    ### Patch Changes
    
    - @ctx-core/object: ^28.1.0 -> ^28.2.0
    - Updated dependencies
    - @ctx-core/nanostores@6.0.12
    
    ## 9.1.26
    
    ### Patch Changes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,13 @@
 # @ctx-core/event-log
 
+## 9.1.27
+
+### Patch Changes
+
+- @ctx-core/object: ^28.1.0 -> ^28.2.0
+- Updated dependencies
+  - @ctx-core/nanostores@6.0.12
+
 ## 9.1.26
 
 ### Patch Changes
--- src/event_log/index.test.ts
+++ src/event_log/index.test.ts
@@ -1,9 +1,9 @@
-import { ctx_ } from '@ctx-core/object'
+import { ctx__new } from '@ctx-core/object'
 import { test } from 'uvu'
 import { equal } from 'uvu/assert'
 import { event_log_, event_log__add, event_log__set_limit } from '../index.js'
 test('event_log_', ()=>{
-	const ctx = ctx_()
+	const ctx = ctx__new()
 	equal(event_log_(ctx), [])
 	event_log__add(ctx, { msg: 'msg0' })
 	equal(event_log_(ctx), [{ msg: 'msg0' }])
